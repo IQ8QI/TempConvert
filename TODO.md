137a81f7ac3bf4735c141576be45051c86bbd3ac
Bartłomiej Konecki <bartekkonecki97@gmail.com>
2022-08-18 11:49:00 +0200

    Decimal point update
    Output have 2 deciaml points by default. User can decide how many decimal points he want.

--- TODO.md
+++ TODO.md
@@ -4,7 +4,8 @@ DONE - Make it possible that temperature and units can be put in random order
 
 DONE - Create checking for valid input, with error messages when user try to convert strings
 
-DONE - Parameters with doubl and single '-' are ok
+DONE - Parameters with double and single '-' are ok
 
-Make output to always have 2 decimal points
+DONE - Make output to default have 2 decimal points
 
+DONE - Create possibility to add --precision parameter to decide how many decimal points user want in output
--- src/TempConvert.java
+++ src/TempConvert.java
@@ -7,81 +7,87 @@ public class TempConvert
     {
 
         //If no input, or invalid number of parameters, or user asks for help, then show help and terminate
-        if(args.length != 3 || args[0].equals("-h") || args[0].equals("--help") || args[0].equals("?") || args[0].equals(""))
+        if( !(args.length == 3 || args.length == 5) || args[0].equals("-h") || args[0].equals("--help") || args[0].equals("?") || args[0].equals(""))
         {
             help();
         }
 
-        //Get the parameters, and add them to params list
+        //Get the input and output unit parameters, and add them to units list
         //Get temperature and add it to inputTemp
+        //Get precision
         float inputTemp = Float.NaN;
-        List<String> params = new ArrayList<>(2);
-        for (String arg : args)
-            if (arg.charAt(0) == '-')
-                params.add(arg);
-            else {
-                try {
-                    inputTemp = Float.parseFloat(arg);
-                } catch (Exception e) {
-                    unknownInput(arg);
-                }
-            }
-
-        /*Not needed???
-        if(Float.isNaN(inputTemp))
+        int precision = 2;
+        List<String> units = new ArrayList<>(2);
+        for(int i = 0; i < args.length; i++)
         {
-            unknownInput(args[i]);
+            if(args[i].equals("--precision") || args[i].equals("-p") || args[i].equals("-precision") || args[i].equals("--p")) {
+                try {
+                    precision = Integer.parseInt(args[i + 1]);
+                    if(precision < 0)
+                        throw new Exception();
+                } catch (Exception e) {
+                    unknownInput(args[i+1]);
+                }
+                i++;
+            } else if (args[i].charAt(0) == '-')
+                units.add(args[i]);
+            else
+                try {
+                    inputTemp = Float.parseFloat(args[i]);
+                } catch (Exception e) {
+                    unknownInput(args[i]);
+                }
         }
-         */
+
 
         //Decide what kind of conversion operation should be done on input temperature
-        if(params.get(0).equals("-c") || params.get(0).equals("--celsius") || params.get(0).equals("--c") || params.get(0).equals("-celsius"))
+        if(units.get(0).equals("-c") || units.get(0).equals("--celsius") || units.get(0).equals("--c") || units.get(0).equals("-celsius"))
         {
-            if(params.get(1).equals("-f") || params.get(1).equals("--fahrenheit") || params.get(1).equals("--f") || params.get(1).equals("-fahrenheit"))
+            if(units.get(1).equals("-f") || units.get(1).equals("--fahrenheit") || units.get(1).equals("--f") || units.get(1).equals("-fahrenheit"))
             {
-                System.out.println(Convert.celsiusToFahrenheit(inputTemp));
+                System.out.printf("%." + precision + "f\n", Convert.celsiusToFahrenheit(inputTemp));
                 System.exit(0);
             }
-            else if(params.get(1).equals("-k") || params.get(1).equals("--kelvin") || params.get(1).equals("--k") || params.get(1).equals("-kelvin"))
+            else if(units.get(1).equals("-k") || units.get(1).equals("--kelvin") || units.get(1).equals("--k") || units.get(1).equals("-kelvin"))
             {
-                System.out.println(Convert.celsiusToKelvin(inputTemp));
+                System.out.printf("%." + precision + "f\n", Convert.celsiusToKelvin(inputTemp));
                 System.exit(0);
             }
             else
-                unknownInput(params.get(1));
+                unknownInput(units.get(1));
         }
-        else if(params.get(0).equals("-f") || params.get(0).equals("--fahrenheit") || params.get(0).equals("--f") || params.get(0).equals("-fahrenheit"))
+        else if(units.get(0).equals("-f") || units.get(0).equals("--fahrenheit") || units.get(0).equals("--f") || units.get(0).equals("-fahrenheit"))
         {
-            if(params.get(1).equals("-c") || params.get(1).equals("--celsius") || params.get(1).equals("--c") || params.get(1).equals("-celsius"))
+            if(units.get(1).equals("-c") || units.get(1).equals("--celsius") || units.get(1).equals("--c") || units.get(1).equals("-celsius"))
             {
-                System.out.println(Convert.fahrenheitToCelsius(inputTemp));
+                System.out.printf("%." + precision + "f\n", Convert.fahrenheitToCelsius(inputTemp));
                 System.exit(0);
             }
-            else if(params.get(1).equals("-k") || params.get(1).equals("--kelvin") || params.get(1).equals("--k") || params.get(1).equals("-kelvin"))
+            else if(units.get(1).equals("-k") || units.get(1).equals("--kelvin") || units.get(1).equals("--k") || units.get(1).equals("-kelvin"))
             {
-                System.out.println(Convert.fahrenheitToKelvin(inputTemp));
+                System.out.printf("%." + precision + "f\n", Convert.fahrenheitToKelvin(inputTemp));
                 System.exit(0);
             }
             else
-                unknownInput(params.get(1));
+                unknownInput(units.get(1));
         }
-        else if(params.get(0).equals("-k") || params.get(0).equals("--kelvin") || params.get(0).equals("--k") || params.get(0).equals("-kelvin"))
+        else if(units.get(0).equals("-k") || units.get(0).equals("--kelvin") || units.get(0).equals("--k") || units.get(0).equals("-kelvin"))
         {
-            if(params.get(1).equals("-f") || params.get(1).equals("--fagrenheit") || params.get(1).equals("--f") || params.get(1).equals("-fahrenheit"))
+            if(units.get(1).equals("-f") || units.get(1).equals("--fagrenheit") || units.get(1).equals("--f") || units.get(1).equals("-fahrenheit"))
             {
-                System.out.println(Convert.kelvinToFahrenheit(inputTemp));
+                System.out.printf("%." + precision + "f\n", Convert.kelvinToFahrenheit(inputTemp));
                 System.exit(0);
             }
-            else if(params.get(1).equals("-c") || params.get(1).equals("--celsius") || params.get(1).equals("--c") || params.get(1).equals("-celsius"))
+            else if(units.get(1).equals("-c") || units.get(1).equals("--celsius") || units.get(1).equals("--c") || units.get(1).equals("-celsius"))
             {
-                System.out.println(Convert.kelvinToCelsius(inputTemp));
+                System.out.printf("%." + precision + "f\n", Convert.kelvinToCelsius(inputTemp));
                 System.exit(0);
             }
             else
-                unknownInput(params.get(1));
+                unknownInput(units.get(1));
         }
         else
-            unknownInput(params.get(0));
+            unknownInput(units.get(0));
 
     }
 
@@ -91,18 +97,22 @@ public class TempConvert
         System.out.println(
                 """
                         NAME
-                        \tTempConvert - converts temperature units
+                        \tTempConvert - converts temperature value between units
                         SYNTAX
-                        \tTempConvert --[input unit] --[output unit] [temperature]
+                        \tTempConvert --[input unit] --[output unit] [temperature] {--precision *precision_value*}
                         DESCRIPTION
-                        \tConverts temperature between commonly used units. Currently supported are operations between celsius, kelvin and fahrenheit
+                        \tConverts temperature between commonly used units and outputs result in command line. Currently supported are operations between celsius, kelvin and fahrenheit.
+                        -c, --celsius - conversion from/to celsius
+                        -f, --fahrenheit - conversion from/to fahrenheit
+                        -k, --kelvin - conversion from/to kelvin
+                        -p, --precision - specify number of decimal points in the result
                         Example usage:
-                        TempConvert -c -f 18
-                        TempConvert --celsius --kelvin 32
+                        TempConvert -c -f 18 -p 2
+                        TempConvert --celsius --kelvin 32 --precision 5
                         AUTHOR
                         \tWrote by Bartłomej Konecki
                         BUG REPORT
-                        \tTODO"""
+                        \thttps://github.com/IQ8QI/TempConvert"""
         );
         System.exit(0);
     }
@@ -110,12 +120,13 @@ public class TempConvert
     //Error message giving the place were is the unknown parameter
     public static void unknownInput(String input)
     {
-        System.out.println("ERROR\nUnknown input format at:" + input + "\nExample usage:\nTempConvert -c -k 28.5");
+        System.out.println("ERROR\nUnknown input format at:" + input + "\nExample usage:\nTempConvert -c -k 28.5\nTempConvert --fahrenheit --kelvin 13 --precision 4");
         System.exit(-1);
     }
 
 }
 
+//Class Convert contain methods to convert temperature between units
 class Convert
 {
     public static float celsiusToFahrenheit(float input)
